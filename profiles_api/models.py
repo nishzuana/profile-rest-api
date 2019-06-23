9bfd80b864cebfccdab5199ff00db0dfde1d7f6b
nishu <nishu.ssh@gmail.com>
2019-06-24 00:49:31 +0530

    Added HelloAPIView

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth.models import User
 
 
 class UserProfileManager(BaseUserManager):
@@ -16,7 +17,7 @@ class UserProfileManager(BaseUserManager):
         user.set_password(password)
         user.save(using=self.db)
 
-        return users
+        return user
 
     def create_superuser(self, email, name, password):
         """ Create and save a new superuser with given details"""
